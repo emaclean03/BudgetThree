44316c346235303804c3afa7d2ced7383e59cf9b
Eric <>
2022-08-13 10:36:46 -0400

    Some fun css stuff

--- resources/js/Components/Input.vue
+++ resources/js/Components/Input.vue
@@ -2,7 +2,7 @@
   <input
     ref="input"
     :class="[
-      'py-2 border-gray-400 rounded-md',
+      'py-0 border-gray-400 rounded-md',
       'focus:border-gray-400 focus:ring focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-white',
       'dark:border-gray-600 dark:bg-dark-eval-1 dark:text-gray-300 dark:focus:ring-offset-dark-eval-1',
       {
@@ -17,7 +17,6 @@
 
 <script setup>
 import { onMounted, ref } from 'vue'
-
 
 
 defineProps({
